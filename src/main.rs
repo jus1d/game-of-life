1fbdc9efe51f59660faf7d2ee39588f3125c8c09
jus1d <artfa63@gmail.com>
2024-03-09 21:11:50 +0400

    chore: Add field.display()

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+const ROWS: usize = 10;
+const COLS: usize = 20;
+
+#[derive(Clone, Copy, Debug)]
+enum Cell {
+    Dead,
+    Alive,
+}
+
+struct Field {
+    cells: [[Cell; COLS]; ROWS]
+}
+
+impl Field {
+    fn new() -> Self {
+        Field {
+            cells: [[Cell::Dead; COLS]; ROWS],
+        }
+    }
+
+    fn display(&self) {
+        for row in &self.cells {
+            for &cell in row {
+                match cell {
+                    Cell::Dead => print!("."),
+                    Cell::Alive => print!("#"),
+                }
+            }
+            println!();
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut field = Field::new();
+    field.display();
 }
